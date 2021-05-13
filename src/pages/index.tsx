622df692c7f75d70446fcce8d37d067231a2bee1
Thales Testoni <thalestestoni@gmail.com>
2021-05-12 22:06:47 -0300

    Título dinâmico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import Head from 'next/head';
+
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello World
-    </h1>
-  )
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+
+      <h1 className={styles.title}>
+        Hello World
+      </h1>
+    </>
+  );
 }
