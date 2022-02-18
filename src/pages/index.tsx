849e21dbdb65f4285b182e286924fe00245cbdd8
Thales Testoni <thalestestoni@gmail.com>
2022-02-18 14:13:58 -0300

    Testing Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ import styles from './home.module.scss';
 interface HomeProps {
   product: {
     priceId: string,
-    amount: number;
+    amount: string;
   }
 }
 
@@ -29,7 +29,7 @@ export default function Home({ product }: HomeProps) {
             Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId}/>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -44,7 +44,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const product = {
     priceId: price.id,
     amount: new Intl.NumberFormat('en-US', {
-      style: 'currency', 
+      style: 'currency',
       currency: 'USD'
     }).format(price.unit_amount / 100)
   }
